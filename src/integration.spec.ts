8a1cd74933dbca5ea6fa98f6c6c419c0a227852b
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-07-30 23:02:52 -0300

    tests: update integration tests names

--- src/integration.spec.ts
+++ src/integration.spec.ts
@@ -3,16 +3,16 @@ import app from './app';
 import server from './server';
 
 describe('Integration', () => {
-  it('Should be able to connect to server', async () => {
-    const response = await request(app).get('/api/ping');
-
-    expect(response.status).toBe(200);
-    expect(response.body.message).toBe('pong');
-  });
-
   it('Should be able to start and close the server', async () => {
     expect(server.listening).toBe(true);
 
     server.close();
+  });
+
+  it('Should be able to make a request and get a', async () => {
+    const response = await request(app).get('/api/ping');
+
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('pong');
   });
 });
