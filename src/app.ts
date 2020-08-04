f56096c420b3f29ed124f1a2dca50036307cae1d
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-08-04 17:38:15 -0300

    improv: add some middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import 'dotenv/config';
 import express from 'express';
+import compression from 'compression';
+import helmet from 'helmet';
 import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 
 import errorHandlerMiddleware from './middlewares/errorHandler';
 
@@ -8,9 +11,18 @@ import router from './router';
 
 const app = express();
 
+app.use(compression());
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 60,
+  max: 100,
+});
+
+app.use(limiter);
 
 app.use('/api', router);
 
